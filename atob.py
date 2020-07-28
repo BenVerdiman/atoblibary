184fb540511b3d5b3c894302c1b3cca66cf332e5
BenVerdiman <49784615+BenVerdiman@users.noreply.github.com>
2020-07-28 13:18:40 +0300

    Update atob.py

--- atob.py
+++ atob.py
@@ -1,8 +1,7 @@
 def atob(a,b):
-    print(a,b)
     tmp = 0
     tmp = a #Прехвърляме информацията на а в темп
     a=b #Прехвърляме б в а
     b=tmp #Прехвъляме а в б
-    print(a,b)
+    print("Variables is changed", "a=",a," ,b=",b)
     
